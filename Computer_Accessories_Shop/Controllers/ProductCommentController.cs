fbe3af69c2d04e6276c8368957eb669ecb318ecd
pouyatirdad <pouyatirdad70@gmail.com>
2022-05-05 15:32:03 +0430

    product comment

---
+++ Computer_Accessories_Shop/Controllers/ProductCommentController.cs
@@ -0,0 +1,77 @@
+﻿using Computer_Accessories_Shop.Api.ViewModel.Products;
+using Computer_Accessories_Shop.Data.Model;
+using Computer_Accessories_Shop.Service.Service;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Computer_Accessories_Shop.Api.Controllers
+{
+    [ApiController]
+    [EnableCors("MyPolicy")]
+    [Route("[controller]")]
+    public class ProductCommentController : Controller
+    {
+        private readonly IProductCommentService ProductCommentService;
+        public ProductCommentController(IProductCommentService ProductCommentService)
+        {
+            this.ProductCommentService = ProductCommentService;
+        }
+
+        [HttpGet]
+        public IEnumerable<ProductComment> Get()
+        {
+            return ProductCommentService.GetAll();
+        }
+        [HttpGet("Get/{id:int}")]
+        public ProductComment Get(int id)
+        {
+            var findedReslut = ProductCommentService.GetById(id);
+            return findedReslut;
+        }
+        [HttpPost("Create")]
+        public bool Create(ProductCommentViewModel model)
+        {
+            if (model.ParentId == 0)
+                model.ParentId = null;
+
+            var newModel=new ProductComment()
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Message = model.Message,
+                AddedDate = model.AddedDate,
+                Rate = model.Rate,
+                ParentId = model.ParentId,
+                ProductID = model.ProductID,
+            };
+
+            return ProductCommentService.Create(newModel);
+        }
+        [HttpPost("Edit")]
+        public bool Edit(ProductCommentViewModel model)
+        {
+            if (model.ParentId == 0)
+                model.ParentId = null;
+
+            var newModel = new ProductComment()
+            {
+                ID = model.ID,
+                Name = model.Name,
+                Email = model.Email,
+                Message = model.Message,
+                AddedDate = model.AddedDate,
+                Rate = model.Rate,
+                ParentId=model.ParentId,
+                ProductID=model.ProductID,
+            };
+
+            return ProductCommentService.Edit(newModel);
+        }
+        [HttpPost("Delete")]
+        public bool Delete(int id)
+        {
+            return ProductCommentService.Delete(id);
+        }
+    }
+}
---
+++ Computer_Accessories_Shop/ViewModel/Products/ProductCommentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Computer_Accessories_Shop.Api.ViewModel.Products
+{
+    public class ProductCommentViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public DateTime? AddedDate { get; set; }
+        public int? Rate { get; set; }
+        public int? ParentId { get; set; }
+        public int ProductID { get; set; }
+
+    }
+}
